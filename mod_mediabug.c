5250f9cf735d7f5e4388e61ef73882300345be58
zhanwang-sky <zhanwang-sky@users.noreply.github.com>
2025-03-03 16:36:43 +0800

    create module interface

--- mod_mediabug.c
+++ mod_mediabug.c
@@ -12,13 +12,18 @@ SWITCH_MODULE_SHUTDOWN_FUNCTION(mod_mediabug_shutdown);
 SWITCH_MODULE_RUNTIME_FUNCTION(mod_mediabug_runtime);
 SWITCH_MODULE_DEFINITION(mod_mediabug, mod_mediabug_load, mod_mediabug_shutdown, NULL);
 
+/* Macro expands to: switch_status_t mod_my_echo_load(switch_loadable_module_interface_t **module_interface, switch_memory_pool_t *pool) */
 SWITCH_MODULE_LOAD_FUNCTION(mod_mediabug_load) {
+  /* connect my internal structure to the blank pointer passed to me */
+  *module_interface = switch_loadable_module_create_module_interface(pool, modname);
+
   switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_INFO, "%s loaded\n", modname);
+
   return SWITCH_STATUS_SUCCESS;
 }
 
 SWITCH_MODULE_SHUTDOWN_FUNCTION(mod_mediabug_shutdown) {
-  switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_INFO, "%s shutdown\n", modname);
+  switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_INFO, "shutting down %s\n", modname);
   return SWITCH_STATUS_SUCCESS;
 }
 
